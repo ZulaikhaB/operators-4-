0cdb722137e7c15017ddf93c3df573074b1684c0
ZulaikhaB <bestwishes88@hotmail.com>
2025-12-28 11:41:37 +0000

    Add files via upload

---
+++ operators4.3.py
@@ -0,0 +1,6 @@
+I = int(input("enter the numerator:"))
+S = int(input("enter the denominator :"))
+if I % S == 0 :
+    print("the numerator is divisible by the denominator")
+else :
+    print("the numerator is not divisible")
